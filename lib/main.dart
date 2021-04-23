2cbe01c6beddf6a46f54066ca27b6a48471fe54d
jeissonsantacruz <jeissonsantacruz@javerianacali.edu.co>
2021-04-23 16:47:26 -0500

    se agrego sesion unico para el usario

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:userapp/aplicacion/paginas/home_page.dart';
+import 'package:userapp/aplicacion/paginas/login_page.dart';
 import 'package:userapp/aplicacion/paginas/splash_page.dart';
 import 'package:userapp/arquitectura/usuario_preferencias.dart';
 import 'package:userapp/arquitectura/usuario_provider.dart';
+import 'package:provider/provider.dart';
 
 
 void main() async {
@@ -20,22 +23,32 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final GlobalKey<NavigatorState> navigatorKey =
       new GlobalKey<NavigatorState>();
+  final HttpLink httpLink =
+      HttpLink(uri: 'https://graphqlzero.almansi.me/api');
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
+    final ValueNotifier<GraphQLClient> client =
+        new ValueNotifier<GraphQLClient>(
+      GraphQLClient(cache: InMemoryCache(), link: httpLink),
+    );
+    return GraphQLProvider(
+      client: client,
+      child: MultiProvider(
         providers: [
-          ChangeNotifierProvider(builder: (context) => ProvUsuario()),
+          ChangeNotifierProvider(create: (context) => ProvUsuario()),
         ],
-        child: MaterialApp(
+              child: MaterialApp(
           debugShowCheckedModeBanner: false,
           initialRoute: ruta(),
           navigatorKey: navigatorKey,
           routes: {
-            // '/homepage': (context) => HomePage(),
-            // '/loginpage': (context) => LoginPage(),
+            '/homepage': (context) => HomePage(),
+            '/loginpage': (context) => LoginPage(),
             '/splashpage': (context) => SplashPage(),
           },
-        ));
+        ),
+      ),
+    );
   }
 
   ruta<String>() {
